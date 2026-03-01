711eae20e42c398cb3afca1ad90621735fadce26
EDebbuger <locktermac@gmail.com>
2026-03-01 09:28:30 +0100

    frontend stuff

---
+++ script.js
@@ -0,0 +1,18 @@
+//webShop skript
+//Här går in fetch requests
+let variable = undefined;
+
+class productPage {
+
+    constructor(image,title,description,price) {
+        this.image=image;
+        this.title=title;
+        this.description=description;
+        this.price=price;
+    }
+
+    createPage() {
+        
+    }
+}
+
